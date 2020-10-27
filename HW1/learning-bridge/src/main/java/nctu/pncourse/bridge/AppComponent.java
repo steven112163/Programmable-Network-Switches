613cd288a7123a6d2978bf9e40616f089279c0ee
steven112163 <steven112163@gmail.com>
2020-10-27 23:42:06 +0800

    Implement hw1 framework

--- HW1/learning-bridge/src/main/java/nctu/pncourse/bridge/AppComponent.java
+++ HW1/learning-bridge/src/main/java/nctu/pncourse/bridge/AppComponent.java
@@ -16,7 +16,9 @@
 package nctu.pncourse.bridge;
 
 import com.google.common.collect.ImmutableSet;
+import org.onlab.packet.Ethernet;
 import org.onosproject.cfg.ComponentConfigService;
+import org.onosproject.net.packet.PacketPriority;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -31,6 +33,27 @@ import java.util.Dictionary;
 import java.util.Properties;
 
 import static org.onlab.util.Tools.get;
+
+/** Libraries for hw */
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.Path;
+
+import org.onosproject.net.packet.PacketService;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketContext;
+
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flow.FlowRuleService;
+
+import org.onosproject.net.topology.TopologyService;
+
+import org.onosproject.net.host.HostService;
+
+import java.util.Set;
 
 /**
  * Skeletal ONOS application component.
@@ -50,16 +73,75 @@ public class AppComponent implements SomeInterface {
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected ComponentConfigService cfgService;
 
+    /** Services for hw */
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected CoreService coreService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected PacketService packetService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected FlowRuleService flowRuleService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected HostService hostService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected TopologyService topologyService;
+
+    // App ID
+    private ApplicationId appId;
+
+    // Packet processor
+    private BridgeProcessor processor = new BridgeProcessor();
+
     @Activate
     protected void activate() {
         cfgService.registerProperties(getClass());
+        appId = coreService.registerApplication("nctu.pncourse.bridge");
+        packetService.addProcessor(processor, PacketProcessor.director(2));
+        requestsPackets();
         log.info("Started");
     }
 
     @Deactivate
     protected void deactivate() {
         cfgService.unregisterProperties(getClass(), false);
+        flowRuleService.removeFlowRulesById(appId);
+        packetService.removeProcessor(processor);
+        processor = null;
+        cancelPackets();
         log.info("Stopped");
+    }
+
+    /**
+     * Request Packet-in via PacketService
+     */
+    private void requestsPackets() {
+        // Get all IPv4 packets
+        TrafficSelector.Builder selector = DefaultTrafficSelector.builder()
+                .matchEthType(Ethernet.TYPE_IPV4);
+        packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);
+
+        // Get all ARP packets
+        selector = DefaultTrafficSelector.builder()
+                .matchEthType(Ethernet.TYPE_ARP);
+        packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);
+    }
+
+    /**
+     * Cancel requested Packet-in via packet service
+     */
+    private void cancelPackets() {
+        // Cancel all IPV4 packets
+        TrafficSelector.Builder selector = DefaultTrafficSelector.builder()
+                .matchEthType(Ethernet.TYPE_IPV4);
+        packetService.cancelPackets(selector.build(), PacketPriority.REACTIVE, appId);
+
+        // Cancel all ARP packets
+        selector = DefaultTrafficSelector.builder()
+                .matchEthType(Ethernet.TYPE_ARP);
+        packetService.cancelPackets(selector.build(), PacketPriority.REACTIVE, appId);
     }
 
     @Modified
@@ -74,6 +156,65 @@ public class AppComponent implements SomeInterface {
     @Override
     public void someMethod() {
         log.info("Invoked");
+    }
+
+    private class BridgeProcessor implements PacketProcessor {
+        /**
+         * Process the packets
+         *
+         * @param context content of the incoming message
+         */
+        @Override
+        public void process(PacketContext context) {
+
+        }
+
+        /**
+         * Output packet from the port
+         *
+         * @param context content of the incoming message
+         * @param port output port number
+         */
+        private void packet_out(PacketContext context, PortNumber port) {
+            context.treatmentBuilder().setOutput(port);
+            context.send();
+        }
+
+        /**
+         * Flood the packet
+         *
+         * @param context content of the incoming packet
+         */
+        private void flood(PacketContext context) {
+            if (topologyService.isBroadcastPoint(topologyService.currentTopology(), context.inPacket().receivedFrom()))
+                packet_out(context, PortNumber.FLOOD);
+            else
+                context.block();
+        }
+
+        /**
+         * Pick a path that doesn't lead back to where the packet came from
+         *
+         * @param paths all paths that lead to the destination
+         * @param port the port where packet came from
+         */
+        private Path pick_forward_path_if_possible(Set<Path> paths, PortNumber port) {
+            for (Path path : paths) {
+                if (!path.src().port().equals(port))
+                    return path;
+            }
+            return null;
+        }
+
+        /**
+         * Install flow rules on a switch
+         *
+         * @param context content of the incoming packet
+         * @param port output port to be defined in the flow rule
+         */
+        private void install_rule(PacketContext context, PortNumber port) {
+
+        }
     }
 
 }
